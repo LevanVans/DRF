1bddb64910ccb765a136cd46179a57875df30662
LevanVans <levanesha@gmail.com>
2023-08-31 12:08:16 +0400

    Added api_view decorator and ModelSerializer

--- drf/backend/products/models.py
+++ drf/backend/products/models.py
@@ -8,3 +8,10 @@ class Product(models.Model):
     content = models.TextField(blank=True, max_length=500)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
     
+    
+    @property
+    def sale_price(self):
+        return self.price - 1
+    
+    def get_discount(self):
+        return self.price + 100
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -17,3 +17,23 @@ print("-"*20)
 print(get_response.json())
 
 print(get_response.status_code)
+
+
+
+class Hello():
+    def __init__(self) -> None:
+        pass
+    
+    @property
+    def NN(self):
+        return 2123
+    @property
+    def n2(self):
+        return 1231312
+    
+m = Hello()
+    
+    
+    
+    
+print(m.n2)
